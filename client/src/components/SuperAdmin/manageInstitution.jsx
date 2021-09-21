e5c0662b044408101818038e681635e7c7ec4d25
arif <arif.ce2020@gmail.com>
2021-09-20 22:42:24 -0500

    form submission works

--- client/src/components/SuperAdmin/manageInstitution.jsx
+++ client/src/components/SuperAdmin/manageInstitution.jsx
@@ -1,41 +1,96 @@
 import React from "react";
 import "../index.css";
-import { Button } from "reactstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Link } from "react-router-dom";
 
-export default function manageInstitution() {
-  return (
-    <div class="body">
-      <header class="bodyheader">
-        <h1>EDUBD</h1>
-      </header>
-      <p>Super Admin View</p>
-      <p>Add New Institution</p>
+class addNewInstitution extends React.Component {
+  constructor() {
+    super();
 
-      <form class="form">
-        <label>
-          Name:
-          <input type="text" />
-        </label>
-        <label>
-          Address:
-          <input type="text" />
-        </label>
-        <label>
-          Phone:
-          <input type="tel" />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+    this.state = {
+      institutionname: {},
+      institutionaddress: {},
+      institutionphone: {},
+    };
+    this.onSubmit = this.handleSubmit.bind(this);
+  }
 
-      <table class="table">
-        <tr>
-          <td>Name</td>
-          <td>Address</td>
-          <td>Phone</td>
-        </tr>
-      </table>
-    </div>
-  );
+  handleSubmit(event) {
+    event.preventDefault();
+
+    const data = {
+      institutionname: this.state.institutionname,
+      institutionaddress: this.state.institutionaddress,
+      institutionphone: this.state.institutionphone,
+    };
+
+    fetch("http://localhost:3000/manageInstitution", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => res.json())
+      .catch((error) => console.error("Error: ", error))
+      .then((response) => console.log("Success: ", response));
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value.trim(),
+    });
+  };
+
+  render() {
+    return (
+      <div class="body">
+        <header class="bodyheader">
+          <h1>EDUBD</h1>
+        </header>
+        <p>Super Admin View</p>
+        <p>Add New Institution</p>
+
+        {/* for the form, you will need the name tag in the inputs. that's how this.state.### works at the top */}
+
+        <form class="form" onSubmit={this.onSubmit}>
+          <label htmlFor="institutionname">
+            Institution Name:
+            <input
+              id="institutionname"
+              name="institutionname"
+              type="text"
+              onChange={this.handleChange}
+            />
+          </label>
+          <label htmlFor="institutionaddress">
+            Address:
+            <input
+              id="institutionaddress"
+              name="institutionaddress"
+              type="text"
+              onChange={this.handleChange}
+            />
+          </label>
+          <label htmlFor="institutionphone">
+            Phone:
+            <input
+              id="institutionphone"
+              name="institutionphone"
+              type="tel"
+              onChange={this.handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+
+        <table class="table">
+          <tr>
+            <td>Institution Name</td>
+            <td>Address</td>
+            <td>Phone</td>
+          </tr>
+        </table>
+      </div>
+    );
+  }
 }
+
+export default addNewInstitution;
--- server/server.js
+++ server/server.js
@@ -1,16 +1,60 @@
 const express = require("express");
 const cors = require("cors");
 const db = require("./config/db.config");
+const bodyParser = require("body-parser");
 
 const app = express();
-const port = 8000;
-const table = "eduearth.institution";
+const port = 8000; // this port shows in the browser. not database port
+const database = "eduearth";
+const institutiontable = "institution";
+const userstable = "users";
 
 app.use(cors());
-app.use(express.json());
+app.use(
+  express.json({
+    type: ["application/json", "text/plain"],
+  })
+);
+
+db.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+  db.query(
+    `create table if not exists ${database}.${userstable} (id bigint(8) primary key auto_increment,  name varchar(255) not null )`,
+    function (err, result) {
+      if (err) throw err;
+      console.log("Table created : eduearth.users");
+    }
+  );
+});
 
 app.get("/", (req, res) => {
-  db.query(`SELECT * FROM ${table};`, (err, result) => {
+  db.query(`select * from ${database}.${institutiontable};`, (err, result) => {
+    if (err) {
+      res.send(err);
+    }
+    res.send(result);
+  });
+});
+
+app.post("/manageInstitution", function (req, res) {
+  const institutionname = req.body.institutionname;
+  const institutionaddress = req.body.institutionaddress;
+  const institutionphone = req.body.institutionphone;
+
+  db.query(
+    `insert into ${database}.${institutiontable} set institutionname = ?, instituitionaddress = ?, institutionphone = ?`,
+    [institutionname, institutionaddress, institutionphone],
+    function (err, result) {
+      console.log(institutionname);
+      if (err) console.log(err);
+      res.send("Successfully added institution");
+    }
+  );
+});
+
+app.get("/manageInstitution", (req, res) => {
+  db.query(`select * from ${database}.${institutiontable};`, (err, result) => {
     if (err) {
       res.send(err);
     }
